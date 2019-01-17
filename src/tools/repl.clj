a5677488c23a59d3c16af6474a2d8d1a025fc319
Tim Zallin <tim.zallin@gmail.com>
2019-01-17 15:58:05 +0300

    add dynamic namespace require

--- src/tools/repl.clj
+++ src/tools/repl.clj
@@ -1,6 +1,7 @@
 (ns tools.repl
   (:require
    [clojure.tools.cli :as cli]
+   [clojure.string :as str]
    [nrepl.server :as nrepl]
    [cider.nrepl :as ci]))
 
@@ -9,11 +10,13 @@
     :default 7888
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ["-f" "--fun FUNCTION" "Function to call after repl start"
-    :parse-fn #(resolve (symbol %))]])
+   ["-f" "--fun FUNCTION" "Function to call after repl start"]])
 
 (defn -main [& args]
   (let [{{p :port f :fun} :options :as opts} (cli/parse-opts args cli-options)]
     (nrepl/start-server :port p :handler ci/cider-nrepl-handler)
     (println "started at port " p)
-    (when f (f))))
+    (when f
+      (let [[*ns _] (str/split f #"\/")]
+        (require `[~(symbol *ns)])
+        (apply (resolve (symbol f)) [])))))
