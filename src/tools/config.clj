d0fdd8df2bed609f1a5aebfb14ae904d448bcee3
Tim Zallin <tim.zallin@gmail.com>
2019-01-18 13:48:30 +0300

    config added

---
+++ src/tools/config.clj
@@ -0,0 +1,18 @@
+(ns tools.config)
+
+(defn to-env [k]
+  (-> k
+      name
+      str/upper-case
+      (str/replace  "-" "_")))
+
+(defn *get [cfg]
+  (->> cfg
+       (map (fn [[opt-name {:keys [default required parse-fn]}]]
+              (let [v (or (System/getenv (to-env opt-name))
+                          default)]
+                (if (and required (nil? v))
+                  (throw (Exception. (str "Required env var " opt-name " is not supplied")))
+                  [opt-name (cond-> v
+                              parse-fn parse-fn)]))))
+       (into {})))
